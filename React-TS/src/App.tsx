f1f63412f201477eb08daf3979c33191faea5022
akshat20000 <akshatprashar2710@gmail.com>
2026-05-16 21:56:42 +0530

    tic tac toe

--- React-TS/src/App.tsx
+++ React-TS/src/App.tsx
@@ -1,22 +1,31 @@
 import './App.css'
-import Home from './pages/home'
-import AdminHome from './pages/adminhome'
+// import Home from './pages/home'
+// import AdminHome from './pages/adminhome'
 // import RenderingList from './pages/renderinglists'
+// function App() {
+//   let type=true;
+//   let content;
+//   if(type){
+//     content=<AdminHome/>
+//   }else{
+//     content = <Home/>
+//   }
+//   return (
+//     <>
+//     <h1>Welcome to my Practice</h1>
+//       <div>{content}</div>
+//       <div>
+//       {/* <RenderingList></RenderingList> */}
+//       </div>
+//     </>
+//   )
+// }
+
+import Index from './tic-tac-toe/index'
 function App() {
-  let type=true;
-  let content;
-  if(type){
-    content=<AdminHome/>
-  }else{
-    content = <Home/>
-  }
-  return (
+  return(
     <>
-    <h1>Welcome to my Practice</h1>
-      <div>{content}</div>
-      <div>
-      {/* <RenderingList></RenderingList> */}
-      </div>
+    <Index></Index>
     </>
   )
 }
